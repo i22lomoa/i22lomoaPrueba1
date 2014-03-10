f3334bdf4f595512bbcd263d00ff8bd250c3ffb2
i22lomoa <i22lomoa@uco.es>
2014-03-10 17:05:19 +0100

    Init commit

---
+++ includes/miinclude.h
@@ -0,0 +1,8 @@
+/*Copyright (C) 2014 Ana
+Desarrollado y diseñado por Ana
+
+This program is free software. You can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License.
+
+For more information about the license read the  README.txt file.*/
+
+#include <stdio.h>
